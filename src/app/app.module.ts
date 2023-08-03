a0af01b43f6c41ffd1971b3cb3789bdfd2e252f2
Shis Hossain <shishossain37@gmail.com>
2023-08-03 12:46:17 +0530

    code added

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { ArticlesModule } from './articles/articles.module';
+import { StaticpageModule } from './staticpage/staticpage.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    PagenotfoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    ArticlesModule,
+    StaticpageModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
